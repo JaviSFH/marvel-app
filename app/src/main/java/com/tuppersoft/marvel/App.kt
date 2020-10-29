bfdf37a3f3a07dfb2f12554c7dba0a1a6b8cd9fa
Raul Rodriguez <raulrcs@gmail.com>
2020-10-29 18:34:12 +0100

    Build environment

---
+++ app/src/main/java/com/tuppersoft/marvel/App.kt
@@ -0,0 +1,11 @@
+package com.tuppersoft.marvel
+
+import android.app.Application
+
+/**
+ * Created by Raúl Rodríguez Concepción on 29/10/2020.
+ * Talento Mobile
+ * raulrcs@gmail.com
+ */
+class App: Application()
+
---
+++ app/src/main/java/com/tuppersoft/marvel/features/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.tuppersoft.marvel.features.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.tuppersoft.marvel.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
